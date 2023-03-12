bef1e1f4ac39a0b49bcc92751893655eeeabc36a
Mondy2137 <mateuszxxxsobczak@gmail.com>
2023-03-12 19:09:31 +0100

    fixed sign-up

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,6 +13,17 @@ def create_app():
             template_folder='./templates')
     app.config['SECRET_KEY'] = 'fsjkfsadassff'                          #admin - pswd database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/sampledb'
+
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
 
 
     db.init_app(app)
@@ -32,3 +44,5 @@ def create_database(app):
     with app.app_context():
         db.create_all()
         print('Created Database')
+
+
