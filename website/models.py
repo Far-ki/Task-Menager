ee98f8784a37836e6847217b149856629c6b6c8a
Mondy2137 <mateuszxxxsobczak@gmail.com>
2023-03-12 18:54:35 +0100

    Added db model fo User

--- website/models.py
+++ website/models.py
@@ -1,8 +1,15 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+#class User(db.Model, UserMixin):
+ #   __tablename__ = 'testBase'
+  #  id = db.Column(db.Integer,primary_key = True)
+   # email = db.Column(db.String(150), unique = True)
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'testBase'
-    id = db.Column(db.Integer,primary_key = True)
-    email = db.Column(db.String(150), unique = True)
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
