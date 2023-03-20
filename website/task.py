f38deb5c4656d6c92218617c394f8e04204d4e20
Jakub Gniadek <jakubgniadek87@gmail.com>
2023-03-20 08:09:40 +0100

    bral wiadomosci

--- website/task.py
+++ website/task.py
@@ -13,32 +13,20 @@ def create_task():
     task = Task(name=name, description=description, completed=completed)
     db.session.add(task)
     db.session.commit()
+    flash('Task created!')
     return redirect(url_for('views.home'))
 
-# @task.route('/', methods=['GET'])
-# def get_all_tasks():
-#     user_id = current_user.id
-#     tasks = Task.query.filter_by(user_id=user_id).all()
-#     return render_template('home.html', tasks=tasks)
+@task.route('/tasks/update', methods=['POST'])
+def update_task():
+    task_id = request.form['id']
+    task = Task.query.get_or_404(task_id)
 
-# @task.route('/<int:id>', methods=['GET'])
-# def get_task(id):
-#     user_id = current_user.id
-#     task = Task.query.get(id)
-#     if not task:
-#         return jsonify({'message': 'Task not found!'}), 404
-#     return jsonify(task.serialize())
-# @task.route('/tasks/update', methods='[PUT]')
-# def update_task():
-#     try:
-#         task_id = request.form.get('task_id')
-#         task = Task.query.filter_by(id=task_id, user_id=current_user.id).first()
-#         db.session.delete(task)
-#         db.session.commit()
-#     except:
-#         flash('There is no such record','error')
-#         return redirect(url_for('views.home'))
-#     return redirect(url_for('views.home'))
+    task.name = request.form['name']
+    task.description = request.form['description']
+    task.completed = 'completed' in request.form
+
+    db.session.commit()
+    return redirect(url_for('views.home'))
 
 @task.route('/tasks/delete', methods=['POST'])
 def delete_task():
