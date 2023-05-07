8514084eb0e080c1b379c3660b2efd67e71365c4
Mondy2137 <mateuszxxxsobczak@gmail.com>
2023-05-07 23:54:04 +0200

    revert merge branch callendar

--- website/views.py
+++ website/views.py
@@ -2,13 +2,11 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
 from sqlalchemy import asc
 from . import db
-
 from .models import User,Group,Event,group_membership, Subtask, event_user
 from datetime import datetime
-
 from flask import jsonify
-from datetime import datetime
 import json
+
 views = Blueprint('views',__name__)
 
 
@@ -16,11 +14,10 @@ views = Blueprint('views',__name__)
 def home():
     if current_user.is_authenticated:
         subtasks = {}
-        now = datetime.now()
         personalTop5 = Event.query.filter_by(user_id=current_user.id).filter(Event.start >= datetime.today()).order_by(asc(Event.start)).limit(5).all()
         for event in personalTop5:
-            subtasks[event.id] = Subtask.query.filter_by(event_id=event.id).order_by(asc(Subtask.id)).all()
-        return render_template('home.html',user = current_user,personalTop5=personalTop5,subtasks=subtasks,now=now)
+            subtasks[event.id] = Subtask.query.filter_by(event_id=event.id).all()
+        return render_template('home.html',user = current_user,personalTop5=personalTop5,subtasks=subtasks)
     else:
         return render_template('home.html',user=current_user)
 
@@ -91,12 +88,3 @@ def remove_user_from_event():
   db.session.commit()
 
   return jsonify({'success': True})
-
-@views.route('/update_subtask', methods=['POST'])
-def update_subtask():
-    subtask_id = request.json['id']
-    is_completed = request.json['is_completed']
-    subtask = Subtask.query.get(subtask_id)
-    subtask.is_completed = is_completed
-    db.session.commit()
-    return jsonify({'message': 'Subtask updated successfully.'})
